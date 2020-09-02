d8f8b3dda774151c470bb558ecfb3790e9c35872
Amna Amir <aamna48@outlook.com>
2020-09-02 14:18:29 +0500

    create mvc web app and add controller test

---
+++ MvcMovie/MvcMovie/Controllers/testController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class testController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
