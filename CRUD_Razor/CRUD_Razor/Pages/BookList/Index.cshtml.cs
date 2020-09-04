9dd0ace7d7631c3450da770492762e166522f81f
Amna Amir <aamna48@outlook.com>
2020-09-04 17:18:32 +0500

    eroors

--- CRUD_Razor/CRUD_Razor/Pages/BookList/Index.cshtml.cs
+++ CRUD_Razor/CRUD_Razor/Pages/BookList/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUD_Razor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,25 @@ namespace CRUD_Razor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbcontext _db;
+        public IEnumerable<Book> Books { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
         }
+        //public IndexModel(ApplicationDbcontext db)
+        //{
+        //    _db = db;
+        //}
+
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
+        }
+        //public async Task OnGet()
+        //{
+        //    Books = await _db.Books.ToListAsync();
+        //}
     }
 }
