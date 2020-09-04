67d6b135d8bcd0c5db02e961ddc0d58010a8e5ba
Amna Amir <aamna48@outlook.com>
2020-09-05 00:33:46 +0500

    make OnPost method

--- WebApplication1/WebApplication1/Migrations/20200904192405_MakeBookNameNotNull.Designer.cs
+++ WebApplication1/WebApplication1/Migrations/20200904192405_MakeBookNameNotNull.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication1.Model;
 
 namespace WebApplication1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200904192405_MakeBookNameNotNull")]
+    partial class MakeBookNameNotNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
